3a50c8536c9a9357cee541eef5d4e886017aea60
Sven Gottwald <2502366+svengo@users.noreply.github.com>
2026-01-31 13:35:57 +0100

    Fix command syntax for envsubst in entry-point.sh

--- entry-point.sh
+++ entry-point.sh
@@ -6,7 +6,7 @@ set -e
 . /config.sh
 
 # generate /etc/tor/torrc-defaults
-/usr/bin/envsubst < /etc/tor/torrc-defaults-source > /etc/tor/torrc-defaults
+envsubst < /etc/tor/torrc-defaults-source > /etc/tor/torrc-defaults
 
 # if /data/torrc doesn't exist, use sample
 if [ ! -s /data/torrc ]; then
